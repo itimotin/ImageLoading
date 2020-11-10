72bc86f473dd25a1b18859f52f07652a24625458
Ion Timotin <johntimotin@gmail.com>
2020-11-10 14:50:03 -0500

    working on preview.

--- Parks_UIHelper/Parks_UIHelper/ContentView.swift
+++ Parks_UIHelper/Parks_UIHelper/ContentView.swift
@@ -17,29 +17,45 @@ struct ImageOverlay: View {
     var body: some View {
         ZStack {
             Text(text!)
-                .font(.callout)
+                .font(.largeTitle)
                 .padding(6)
                 .foregroundColor(.white)
         }.background(Color.black)
-        .opacity(0.8)
+        .opacity(0.6)
         .cornerRadius(10.0)
-        .padding(6)
+        .padding(EdgeInsets(top: 6, leading: 20, bottom: 6, trailing: 6))
     }
 }
 
 
 struct ContentView: View {
-//    let discover : Discover?
+    let discover : Discover?
     let url = URL(string: "https://image.tmdb.org/t/p/original/pThyQovXQrw2m0s9x82twj48Jq4.jpg")!
     var body: some View {
-        AsyncImage(url: url
-                    placeholder: Text("Placeholder")
-                    ).aspectRatio(contentMode: .fit)
+//        AsyncImage(url: url
+//                    placeholder: Text("Placeholder")
+//                    ).aspectRatio(contentMode: .fit)
 //            ImageRow(url: "https://i.pinimg.com/originals/b5/1c/d2/b51cd24c04b71bd6f03a4068e366ed5b.jpg")
 //                .frame(width: 200, height: 200, alignment: .center)
                 //.overlay(ImageOverlay(text: discover?.title))
             
             //Spacer()
+        VStack {
+               Image("nice-park")
+                .resizable()
+                .scaledToFit()
+                .overlay(ImageOverlay(text: "Nice Park"), alignment: .bottomLeading)
+            Text("Looking to expore some new territory? This month's top hikes are recomendations from insiders who knows the parks best")
+                .font(.callout)
+                .foregroundColor(.white).padding(3)
+            List (parks){ park in
+                ParkRow(park: park)
+                
+            }
+            Spacer()
+            
+        }
+        .background(Color.black)
 
     }
 }
@@ -47,6 +63,6 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     let discover = Discover(url: "https://i.pinimg.com/originals/b5/1c/d2/b51cd24c04b71bd6f03a4068e366ed5b.jpg", title: "Beautiful View")
     static var previews: some View {
-        ContentView()
+        ContentView(discover: ContentView_Previews().discover)
     }
 }
